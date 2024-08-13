3d71a83ae7658dc75655b4642325bc30c80c5443
msploit0x00 <minasploit1@outlook.com>
2024-08-13 13:42:36 +0300

    clear filter button

--- report/report/report/تقرير_عن_الاعضاء_الموقوفين/تقرير_عن_الاعضاء_الموقوفين.js
+++ report/report/report/تقرير_عن_الاعضاء_الموقوفين/تقرير_عن_الاعضاء_الموقوفين.js
@@ -1,34 +1,41 @@
-// Copyright (c) 2023, ds and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
 frappe.query_reports["تقرير عن الاعضاء الموقوفين"] = {
-	"filters": [
-		{
-			"fieldname": 'tax_id',
-			"label": __('Tax ID'),
-			"fieldtype": "Data",
-			"reqd": 0,
-		},
-		{
-			"fieldname": 'customer',
-			"label": __('ID'),
-			"fieldtype": 'Link',
-			"options": 'Customer',
-			"reqd": 0
-		},
-		{
-			"fieldname": 'customer_name',
-			"label": __('Company Name'),
-			"fieldtype": 'Data',
-			"reqd": 0
-		},
-			
-		{
-			"fieldname": 'Posting_date',
-			"label": __('Date'),
-			"fieldtype": 'Date',
-			"reqd": 0
-		},
-	]
+    "filters": [
+        {
+            "fieldname": 'tax_id',
+            "label": __('Tax ID'),
+            "fieldtype": "Data",
+            "reqd": 0,
+        },
+        {
+            "fieldname": 'customer',
+            "label": __('ID'),
+            "fieldtype": 'Link',
+            "options": 'Customer',
+            "reqd": 0
+        },
+        {
+            "fieldname": 'customer_name',
+            "label": __('Company Name'),
+            "fieldtype": 'Data',
+            "reqd": 0
+        },
+        {
+            "fieldname": 'Posting_date',
+            "label": __('Date'),
+            "fieldtype": 'Date',
+            "reqd": 0
+        },
+    ],
+    onload: function(report) {
+        report.page.add_inner_button(__('Clear Filters'), function() {
+            frappe.query_report.set_filter_value({
+                "tax_id": "",
+                "customer": "",
+                "customer_name": "",
+                "Posting_date": "",
+            });
+  
+            frappe.query_report.refresh();
+        });
+    }
 };
