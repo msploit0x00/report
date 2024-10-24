92638e92331a88cdab476bdf1a999027637723c7
msploit0x00 <minasploit1@outlook.com>
2024-10-24 16:53:53 +0300

    new reports added

---
+++ report/report/report/single_member_data_report2/single_member_data_report2.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, ds and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Single Member Data Report2"] = {
+	"filters": [
+
+	]
+};
